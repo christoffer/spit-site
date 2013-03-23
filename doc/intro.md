697322ba62cef948377ff62ebdd02056ccd191e1
Christoffer Klang <christoffer.klang@gmail.com>
2013-03-23 01:15:27 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to spit-site
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject spit-site "0.1.0-SNAPSHOT"
+  :description "
+    Marshall through a buch of templates render them into a static site.
+    If data exists in the expected locations, it will be used to render
+    the templates.
+
+    Renders templates using Mustache, and uses JSON as data format."
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]])
